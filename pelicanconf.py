75c092bddf17c38e3ea75353b478e1def91eb85e
Vaibhav Mishra <vinu76jsr@gmail.com>
2013-12-20 23:48:20 +0530

    vaibhavmishra.com pelican site initialized

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Vaibhav Mishra'
+SITENAME = u"Vaibhav Mishra's Blog"
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+# LINKS =  (('Pelican', 'http://getpelican.com/'),
+#           ('Python.org', 'http://python.org/'),
+#           ('Jinja2', 'http://jinja.pocoo.org/'),
+#           ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/vinu76jsr'),
+          ('facebook', 'http://facebook.com/mishravaibhav'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+GITHUB_URL = 'http://github.com/vinu76jsr'
+TWITTER_USERNAME = 'vinu76jsr'
